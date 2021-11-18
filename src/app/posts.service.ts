581e2d97d5924f420fbadc931a345d4ad80abadc
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-18 15:50:02 +0500

    Query params in http request.

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { catchError, map } from 'rxjs/operators';
@@ -24,10 +24,15 @@ export class PostsService {
     }
 
     fetchPosts() {
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty');
+        searchParams = searchParams.append('custom', 'key');
+        
         return this.http
         .get<{ [key: string]: Post }>('https://ng-complete-guide-d5f65-default-rtdb.firebaseio.com/posts.json', 
         {
-            headers: new HttpHeaders({"Custom-header": 'Hello'})
+            headers: new HttpHeaders({"Custom-header": 'Hello'}),
+            params: searchParams
         }
         )
         .pipe(map(responseData => {
