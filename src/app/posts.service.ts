5b17e90110c6bd76e8a51348441b13a985e36d11
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-18 15:43:35 +0500

    Headers in http request.

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { catchError, map } from 'rxjs/operators';
@@ -25,7 +25,11 @@ export class PostsService {
 
     fetchPosts() {
         return this.http
-        .get<{ [key: string]: Post }>('https://ng-complete-guide-d5f65-default-rtdb.firebaseio.com/posts.json')
+        .get<{ [key: string]: Post }>('https://ng-complete-guide-d5f65-default-rtdb.firebaseio.com/posts.json', 
+        {
+            headers: new HttpHeaders({"Custom-header": 'Hello'})
+        }
+        )
         .pipe(map(responseData => {
           const postsArray: Post[] = [];
           for (const key in responseData) {
