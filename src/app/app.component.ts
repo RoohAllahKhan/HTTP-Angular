f779525cbc1d7d928dc68a466c7736793cc58a15
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-18 13:36:54 +0500

    Loading indicator in http.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
     .get<{ [key: string]: Post }>('https://ng-complete-guide-d5f65-default-rtdb.firebaseio.com/posts.json')
     .pipe(map(responseData => {
@@ -49,6 +51,7 @@ export class AppComponent implements OnInit {
       return postsArray;
     }))
     .subscribe( posts => {
+      this.isFetching = false;
       this.loadedPosts = posts;
     })
 
